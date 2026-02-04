fb34352d04df9bef24da2fd18516229d9c8bc84e
Matteo Cola <matteo.cola@hotmail.com>
2026-02-04 10:17:41 +0100

    0.1

---
+++ log/logger.py
@@ -0,0 +1,34 @@
+from datetime import datetime
+import inspect
+from typing import List
+import os
+from config import LOG_FILE
+
+def log(*msg: str) -> None:
+    """
+    Logs messages to log.txt, prepending:
+    - timestamp with milliseconds
+    - file name
+    - function name
+    - line number
+    """
+
+    # Caller info (1 level up the stack)
+    frame = inspect.stack()[1]
+    file_name = os.path.basename(frame.filename)
+    func_name = frame.function
+    line_no = frame.lineno
+
+    # Timestamp
+    time_str = datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")[:-3]
+
+    # Prefix
+    prefix = (
+        f"[{time_str}]\t"
+        f"[{file_name} - {func_name}():{line_no}]\t\t"
+    )
+
+    # Write log
+    with open(LOG_FILE, "a", encoding="utf-8") as f:
+        for m in msg:
+            f.write(f"{prefix}{m}\n")
---
+++ main.py
@@ -0,0 +1,2 @@
+from log.logger import log
+log("This is a test log message.")
